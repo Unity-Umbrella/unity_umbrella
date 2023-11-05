fd694476978267ea3a190490e63aea168474b6c5
Fahad Kabir <hmdipto7@gmail.com>
2023-11-05 08:37:58 -0800

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -8,26 +8,27 @@ import StudentDirectory from "./pages/StudentDirectory/StudentDirectory";
 import HouseListing from './pages/HouseListing/HouseListing';
 import LoginPage from "./pages/Login/Login";
 import RegistrationPage from './pages/Registration/Registration';
-
+import ContactUs from './pages/ContactUs/ContactUs';
 
 const theme = createTheme();
 
 function App() {
   return (
-      <Router>
-          <div>
-              <Header />
-              <Routes>
-                  <Route path="/" element={<Home/>}/>
-                  <Route path="/student-directory" element={<StudentDirectory/>}/>
-                  <Route path="/house-listing" element={<HouseListing/>}/>
-                  <Route path="/login" element={<LoginPage/>}/>
-                  <Route path="/registration" element={<RegistrationPage/>}/>
-                  
-                  {/* Add more routes as needed */}
-              </Routes>
-          </div>
-      </Router>
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/student-directory" element={<StudentDirectory />} />
+          <Route path="/house-listing" element={<HouseListing />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/registration" element={<RegistrationPage />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
